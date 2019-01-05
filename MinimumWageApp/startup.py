0b703de8a48c293c05a4fe52ec03a3c7049d13c2
Space Cowboy <spacecowboy@localhost.localdomain>
2019-01-05 16:36:51 -0500

    Try-Catch added to startup script so that makemigrations command will work.

--- MinimumWageApp/startup.py
+++ MinimumWageApp/startup.py
@@ -4,16 +4,19 @@ from .models import StateData
 
 def start_up():
     # Remove all existing data for states
-    StateData.objects.all().delete()
+    try:
+        StateData.objects.all().delete()
 
-    script_parent_dir = os.path.dirname(__file__)
-    state_data_file = os.path.join(script_parent_dir, 'data/StateData.csv')
-    with open(state_data_file) as state_data_csv:
-        state_data = csv.reader(state_data_csv)
-        for counter, row in enumerate(state_data):
-            if counter > 0:
-                state_model = StateData()
-                state_model.state_name = row[0]
-                state_model.hourly_min_wage = row[1]
-                state_model.yearly_living_cost = row[2]
-                state_model.save()
+        script_parent_dir = os.path.dirname(__file__)
+        state_data_file = os.path.join(script_parent_dir, 'data/StateData.csv')
+        with open(state_data_file) as state_data_csv:
+            state_data = csv.reader(state_data_csv)
+            for counter, row in enumerate(state_data):
+                if counter > 0:
+                    state_model = StateData()
+                    state_model.state_name = row[0]
+                    state_model.hourly_min_wage = row[1]
+                    state_model.yearly_living_cost = row[2]
+                    state_model.save()
+    except:
+        print("ERROR: Unable to populate data in database.")
