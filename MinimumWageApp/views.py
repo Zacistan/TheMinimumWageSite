069e812595ff3cff24398d58e057672c6826f720
Zac Robinson <zacharyleerobinson@gmail.com>
2019-01-26 21:36:54 -0500

    Additional updates for Hourly Living Wage. Moved logic for checking if state is valid into the StateData model.

--- MinimumWageApp/views.py
+++ MinimumWageApp/views.py
@@ -24,8 +24,7 @@ def redirect_to_state(request):
                 if len(state) == 2:
                     state = convert_two_letter_state_code(two_letter_code=state.upper())
                 # Exception is thrown if state is not found in database.
-                if not StateData.objects.filter(state_name=state.upper()):
-                    raise Exception('State not found in database.')
+                StateData.check_if_state_is_valid(state)
             except Exception as e:
                 print('An exception was thrown: {0}'.format(e))
                 form.add_error(field='state_name', error="State name is invalid.")
@@ -38,17 +37,21 @@ def redirect_to_state(request):
     return HttpResponseRedirect('/')
 
 def render_state_data(request, state_name):
-    # Exception is thrown if state is not found in database.
+    StateData.check_if_state_is_valid(state_name)
+    # Grab state_data from database
     state_data = (StateData.objects.filter(state_name=state_name.upper()))[0]
+
     # Hourly wage * 40 hours a week * 52 weeks in a year * 2 people
-    annual_min_wage_for_two = state_data.hourly_min_wage * 40 * 52 * 2
-    minwage_livingcost_difference = state_data.yearly_living_cost - annual_min_wage_for_two
+    annual_min_wage = state_data.hourly_min_wage * 40 * 52
+    annual_cost_of_living = state_data.hourly_living_cost * 40 * 52
+    minwage_livingcost_difference = annual_cost_of_living - annual_min_wage
+    state_search_form = StateSearch()
+
     template_variables = {
         'state_name': state_data.state_name.capitalize(),
-        'annual_min_wage_for_two': '${:,.2f}'.format(annual_min_wage_for_two),
+        'annual_min_wage': '${:,.2f}'.format(annual_min_wage),
         'minwage_livingcost_difference': '${:,.2f}'.format(minwage_livingcost_difference),
         'hourly_min_wage': '${:,.2f}'.format(state_data.hourly_min_wage),
-        'annual_family_living_wage': '${:,.2f}'.format(state_data.yearly_living_cost),
+        'hourly_living_cost': '${:,.2f}'.format(state_data.hourly_living_cost),
     }
-    state_search_form = StateSearch()
     return render(request, 'MinimumWageApp/state_data.html', {'state_data': template_variables, 'state_search_form': state_search_form})
