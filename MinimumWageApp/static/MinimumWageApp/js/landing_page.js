fa6ff500e4b44207b3528b7efbde2d7e3772b966
Zac Robinson <zacharyleerobinson@gmail.com>
2019-01-26 21:37:32 -0500

    Added footer to views. Various CSS and HTML fixes.

--- MinimumWageApp/static/MinimumWageApp/js/landing_page.js
+++ MinimumWageApp/static/MinimumWageApp/js/landing_page.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Set div height to window height
-    $(".landing-page").css('height', $(window).height());
-    $(".landing-page").css('width', $(window).width());
+    $(".landing-page .container").css('height', $(window).height());
+    $(".landing-page .container").css('width', $(window).width());
     //$(".landing-page > .container").css('height', $(window).height());
 });
